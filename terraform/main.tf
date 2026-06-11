569689820c1a2f06914629590edbe04e49eaf93b
rayyanahmed26042005 <ahmedrayyan728@gmail.com>
2026-06-12 00:44:43 +0530

    Fix EC2 instance profile argument name in terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -238,7 +238,7 @@ resource "aws_instance" "backend_server" {
   subnet_id            = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
   key_name             = aws_key_pair.deployer.key_name
-  instance_profile     = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   # Bootstraps Docker & runs Backend container automatically
   user_data = <<-EOF
